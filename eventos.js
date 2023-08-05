7a93896790acab28815fa3d94755eb8598a935b2
Joaquin Talice <joaquintalice@gmail.com>
2023-08-04 21:57:09 -0300

    Solución al bug del button y el div

--- eventos.js
+++ eventos.js
@@ -1,9 +1,14 @@
 const div = document.querySelector('div');
-console.log(div)
+const btn = document.querySelector('button');
 
 div.style.padding = '5px';
 div.style.backgroundColor = 'Green';
 
 div.addEventListener('click', () => {
     alert('Hola! Soy el div');
+});
+
+btn.addEventListener('click', (evento) => {
+    alert('Hola!');
+    evento.stopPropagation();
 });
